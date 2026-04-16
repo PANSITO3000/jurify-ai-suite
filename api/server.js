25f844fffee9dced2b5b59e0949046bf78d9a1c0
Juan Diego <franstation59@gmail.com>
2026-04-15 20:44:18 -0500

    asd

--- api/server.js
+++ api/server.js
@@ -1,8 +1,8 @@
-import serverHandler from '../dist/server/index.js';
-
 export default async function handler(req, res) {
   try {
-    await serverHandler(req, res);
+    const { createServer } = await import('../dist/server/index.js');
+    const server = createServer();
+    await server(req, res);
   } catch (error) {
     console.error('Server error:', error);
     res.status(500).json({ error: 'Internal Server Error', details: error.message });
